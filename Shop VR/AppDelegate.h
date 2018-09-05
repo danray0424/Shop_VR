0f5603cceca4d90569a699b41742e961ab44d69a
Dan Ray <dan@ray-solutions.com>
2018-09-05 13:25:52 -0400

    Initial Commit

---
+++ Shop VR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shop VR
+//
+//  Created by Dan Ray on 9/5/18.
+//  Copyright © 2018 Ray Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Shop VR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Shop VR
+//
+//  Created by Dan Ray on 9/5/18.
+//  Copyright © 2018 Ray Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
