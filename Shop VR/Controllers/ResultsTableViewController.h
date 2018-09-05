9926cf3d888bb1af84fe1aca93010798c339eb9f
Dan Ray <dan@ray-solutions.com>
2018-09-05 18:41:34 -0400

    Search result object

--- Shop VR/Controllers/ResultsTableViewController.h
+++ Shop VR/Controllers/ResultsTableViewController.h
@@ -6,8 +6,13 @@
 //  Copyright © 2018 Ray Solutions. All rights reserved.
 //
 
+
 #import <UIKit/UIKit.h>
+#import "Product.h"
+#import "SearchResults.h"
 
 @interface ResultsTableViewController : UITableViewController
+
+@property (nonatomic, retain) SearchResults *results;
 
 @end
